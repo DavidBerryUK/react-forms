f4ed7c1001f1c64134e1cbfc1c1a074ad19a5b93
David Berry <dave.berry@answerdigital.com>
2024-01-23 11:41:46 +0000

    schema tests boolean - added common validation tests

---
+++ src/forms/__tests__/schemaAssertionsBoolean/SchemaBooleanAssertMandatory.spec.ts
@@ -0,0 +1,87 @@
+import FieldBuilder from "../../syntaxSugar/fieldBuilders/FieldBuilder";
+import FormInstance from "../../models/FormInstance";
+import FormSchemaBase from "../../models/FormSchemaBase";
+import IFormSchema from "../../interfaces/form/IFormSchema";
+
+class TestSchema extends FormSchemaBase implements IFormSchema {
+  fields = {
+    mandatoryValue: FieldBuilder.boolean().mandatory().build(),
+  };
+
+  constructor() {
+    super();
+    this.parseFields(this.fields);
+  }
+}
+
+describe("Schema boolean (Mandatory)", () => {
+  test("Assertion - populated - expect to pass validation", () => {
+    // Assign
+    //
+    let form = new FormInstance(new TestSchema());
+    const { mandatoryValue } = form.fieldSchema.fields;
+    form.setValue(mandatoryValue, 10);
+
+    // Act
+    //
+    form = form.validateAll();
+
+    // Assert
+    //
+    expect(form.isValid).toBeTruthy();
+  });
+
+  test("Assertion - default - pass - not that fields exist until they as set with some value, even if it is null / undefiend", () => {
+    //
+    // Assign
+    //
+    let form = new FormInstance(new TestSchema());
+    const { mandatoryValue } = form.fieldSchema.fields;
+
+    //
+    // Act
+    //
+    form = form.validateAll();
+
+    //
+    // Assert
+    //
+    expect(form.isValid).toBeTruthy();
+  });
+
+  test("Assertion - null - expect to fail validation", () => {
+    // Assign
+    //
+    let form = new FormInstance(new TestSchema());
+    const { mandatoryValue } = form.fieldSchema.fields;
+    form.setValue(mandatoryValue, null);
+
+    // Act
+    //
+    form = form.validateAll();
+
+    // Assert
+    //
+    expect(form.isValid).toBeFalsy();
+    expect(form.getField(mandatoryValue)?.validation.isValid).toBeFalsy();
+    expect(form.getField(mandatoryValue)?.validation.validationMessage).toBe("is mandatory");
+  });
+
+  test("Assertion - null - expect to fail validation", () => {
+    // Assign
+    //
+    let form = new FormInstance(new TestSchema());
+    const { mandatoryValue } = form.fieldSchema.fields;
+    form.setValue(mandatoryValue, undefined);
+
+    // Act
+    //
+    form = form.validateAll();
+
+    // Assert
+    //
+    expect(form.isValid).toBeFalsy();
+    expect(form.getField(mandatoryValue)?.validation.isValid).toBeFalsy();
+    expect(form.getField(mandatoryValue)?.validation.validationMessage).toBe("is mandatory");
+  });
+});
---
+++ src/forms/__tests__/schemaAssertionsBoolean/SchemaBooleanAssertPopulated.spec.ts
@@ -0,0 +1,87 @@
+import FieldBuilder from "../../syntaxSugar/fieldBuilders/FieldBuilder";
+import FormInstance from "../../models/FormInstance";
+import FormSchemaBase from "../../models/FormSchemaBase";
+import IFormSchema from "../../interfaces/form/IFormSchema";
+
+class TestSchema extends FormSchemaBase implements IFormSchema {
+  fields = {
+    populatedValue: FieldBuilder.boolean().populated().build(),
+  };
+
+  constructor() {
+    super();
+    this.parseFields(this.fields);
+  }
+}
+
+describe("Schema boolean (Populated)", () => {
+  test("Assertion - populated - expect to pass validation", () => {
+    // Assign
+    //
+    let form = new FormInstance(new TestSchema());
+    const { populatedValue } = form.fieldSchema.fields;
+    form.setValue(populatedValue, 10);
+
+    // Act
+    //
+    form = form.validateAll();
+
+    // Assert
+    //
+    expect(form.isValid).toBeTruthy();
+  });
+
+  test("Assertion - default - pass - not that fields exist until they as set with some value, even if it is null / undefiend", () => {
+    //
+    // Assign
+    //
+    let form = new FormInstance(new TestSchema());
+    const { populatedValue } = form.fieldSchema.fields;
+
+    //
+    // Act
+    //
+    form = form.validateAll();
+
+    //
+    // Assert
+    //
+    expect(form.isValid).toBeTruthy();
+  });
+
+  test("Assertion - null - expect to fail validation", () => {
+    // Assign
+    //
+    let form = new FormInstance(new TestSchema());
+    const { populatedValue } = form.fieldSchema.fields;
+    form.setValue(populatedValue, null);
+
+    // Act
+    //
+    form = form.validateAll();
+
+    // Assert
+    //
+    expect(form.isValid).toBeFalsy();
+    expect(form.getField(populatedValue)?.validation.isValid).toBeFalsy();
+    expect(form.getField(populatedValue)?.validation.validationMessage).toBe("must be populated");
+  });
+
+  test("Assertion - null - undefined - expect to fail validation", () => {
+    // Assign
+    //
+    let form = new FormInstance(new TestSchema());
+    const { populatedValue } = form.fieldSchema.fields;
+    form.setValue(populatedValue, undefined);
+
+    // Act
+    //
+    form = form.validateAll();
+
+    // Assert
+    //
+    expect(form.isValid).toBeFalsy();
+    expect(form.getField(populatedValue)?.validation.isValid).toBeFalsy();
+    expect(form.getField(populatedValue)?.validation.validationMessage).toBe("must be populated");
+  });
+});
